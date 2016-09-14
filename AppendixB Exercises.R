fe0240b9086278eb65c048f125984ae7090132af
maddicowen <madcowen@mac.com>
2016-09-13 18:01:59 -0700

    Update AppendixB Exercises
    Added examples from B.1 and B.2

--- AppendixB Exercises.R
+++ AppendixB Exercises.R
@@ -1,4 +1,19 @@
-#Matrix multiplication, finding inverses of matrices
+#B.1. Help functions
+##help pages provide a description of, usage, arguments, and details about the function or object.
+##also includes the "value" (the output) and some examples
+?mean
+help(mean)
+
+#B.2. Assignment
+#use the arrox " <- " to assign what is on the right of the arrow to what is on the left
+a <- a+1
+a
+b <- a
+b
+#can separate commands with ";"
+a+a;a+b;a <- a+b;a
+
+#B.3.4. Matrix multiplication, finding inverses of matrices
 (M <- matrix(1:4, ncol=2, byrow=T))
 (Minv <- solve(M))
 M %*% Minv
@@ -8,7 +23,7 @@ Minv %*% M
 (Treatment <- factor(rep(c("Ctrl","Med","High"), each=3)))
 stripchart(1:9~Treatment)
 
-#Dependent iterations
+#B.6.2. Dependent iterations
 gens <- 10
 output <- numeric(gens + 1)
 output[1] <- 25
